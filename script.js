663e13d0cb0271745a8881416fdb7d62064b1ad4
Pedro Felippe <pedfelippe@gmail.com>
2020-04-14 16:30:14 -0300

    .mp4 concluido

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ var urlInput = document.querySelector('.input-youtube')
 
 downBtn.addEventListener('click', () => {
   console.log(`URL: ${urlInput.value}`)
+  sendUrl(urlInput.value)
 })
+
+function sendUrl(url){
+  window.location.href = `http://localhost:3001/download?URL=${url}`;
+}
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const express = require ("express")
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
 
-const app = express()
-
-app.get('/', (req, res) => {
-  res.send("Hello.")
-});
-
-
-app.get('/download', (req, res) => {
-  var URL = req.query.URL;
-  res.json({url:URL});
-});
+app.use(cors());  // com o cors() o sv aceita requests
 
 app.listen(3001, () =>{
   console.log("Works.")
 });  
+
+app.get('/download', (req, res) => {
+  var URL = req.query.URL;
+
+
+  res.header('Content-Disposition', 
+  'attachment; filename="video.mp4"');
+  
+  ytdl(URL, {
+    format: 'mp4'
+    }).pipe(res);
+});
